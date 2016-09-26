3d662dd5ce43ac34996e9bf6c3e90b07061d944b
yxzoro <yangxing20107@gmail.com>
2016-09-26 17:35:23 +0800

    .

--- Kivy_andriod/kivy_demo_listview.py
+++ Kivy_andriod/kivy_demo_listview.py
@@ -9,12 +9,12 @@ class MainView(GridLayout):
         kwargs['cols'] = 2
         super(MainView, self).__init__(**kwargs)
 
-
-
         simple_list_adapter = SimpleListAdapter(data=["book name: {0}".format(i) for i in range(10)], cls=Button)
         list_view = ListView(adapter=simple_list_adapter)
         self.add_widget(list_view)
 
+
 if __name__ == '__main__':
     from kivy.base import runTouchApp
+
     runTouchApp(MainView(width=800))
